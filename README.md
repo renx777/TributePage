97fa6b99478a3e9c54d2771ff721f74376df2e5c
renx777 <arenukaprasad@gmail.com>
2018-01-08 03:02:15 +0530

    added license and code of conduct files

--- README.md
+++
@@ -1,2 +0,0 @@
-# TributePage
-a simple tribute page dedicated to the Greatest Painter Of All Vincent Van Gogh
---
+++ js/index.js
@@ -0,0 +1,7 @@
+ // Load this when the DOM is ready
+        $(function () {
+            // You used .myCarousel here. 
+            // That's the class selector not the id selector,
+            // which is #myCarousel
+            $('#myCarousel').carousel();
+        });
